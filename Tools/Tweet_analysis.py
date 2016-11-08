1b031d09d04703c344842678c1eec34589498387
zimmae <maelle.zimmermann@gmail.com>
2016-11-08 18:24:55 -0500

    Added stemming and comments

--- Tools/Tweet_analysis.py
+++ Tools/Tweet_analysis.py
@@ -15,11 +15,19 @@ from sklearn.linear_model import Perceptron
 from sklearn.linear_model import PassiveAggressiveClassifier
 from sklearn.naive_bayes import BernoulliNB, MultinomialNB
 from sklearn.neighbors import KNeighborsClassifier
+from nltk.stem.snowball import SnowballStemmer
 
 stop_list = nltk.corpus.stopwords.words('english')
 lemmatizer = nltk.stem.WordNetLemmatizer()
 punctuation = list(string.punctuation)
 stop_list = stop_list + punctuation +["rt", 'url']
+stemmer = SnowballStemmer("english")
+HillaryWords = ['hillary clinton','hillaryclinton','hilaryclinton','hillari clinton','hilari clinton','hilary clinton','hillary','clinton']
+DonaldWords = ['donald trump','donaldtrump','donald','trump','realdonaldtrump']
+CarsonWords = ['realbencarson','bencarson','carson']
+BushWords = ['jebbush','bush']
+hillary_re = re.compile('|'.join(map(re.escape, HillaryWords)))
+donald_re = re.compile('|'.join(map(re.escape, DonaldWords)))
 
 data = pd.read_csv("../data/Sentiment.csv")
 classifier =[]
@@ -28,20 +36,32 @@ def preprocess(tweet):
         tweet = tweet.lower()
         tweet = " ".join(tweet.split('#'))
         tweet = re.sub(r'#([^\s]+)', r'\1', tweet)
+        tweet = " ".join(tweet.split('@'))
+        tweet = re.sub(r'@([^\s]+)', r'\1', tweet)
         tweet = re.sub('((www\.[^\s]+)|(https://[^\s]+))','URL',tweet)
         tweet = re.sub("http\S+", "URL", tweet)
         tweet = re.sub("https\S+", "URL", tweet)
-        tweet = re.sub('@[^\s]+','AT_USER',tweet)
-        tweet = tweet.replace("AT_USER","")
+        tweet = " ".join(tweet.split(':'))
+        #removes @username from text entirely
+        #tweet = re.sub('@[^\s]+','AT_USER',tweet)
+        #tweet = tweet.replace("AT_USER","")
         tweet = tweet.replace("URL","")
         tweet = tweet.replace(".","")
         tweet = tweet.replace('\"',"")
         tweet = tweet.replace('&amp',"")
-        tweet  = " ".join([word for word in tweet.split(" ") if word not in stop_list])
-        tweet  = " ".join([word for word in tweet.split(" ") if re.search('^[a-z]+$', word)])
+        #remove punctuation words
+        tweet = " ".join([word for word in tweet.split(" ") if word not in stop_list])
+        #remove words ending with special character
+        tweet = " ".join([word for word in tweet.split(" ") if re.search('^[a-z]+$', word)])
+        #remove common words such as "the"
         tweet = " ".join([lemmatizer.lemmatize(word) for word in tweet.split(" ")])
+        #stem similar words such as "hurt" and "hurting"
+        tweet = " ".join([stemmer.stem(word) for word in tweet.split(" ")])
         tweet = re.sub('[\s]+', ' ', tweet)
         tweet = tweet.strip('\'"')
+        #manually stem similar political words
+        tweet = hillary_re.sub("hillary", tweet)
+        tweet = donald_re.sub("donald", tweet)
     else:
         tweet=''
     return tweet
@@ -55,5 +75,11 @@ data = data.drop_duplicates(subset='processed_text', keep='last')
 categories = data.sentiment.unique()
 categories  = categories.tolist()
 
+#set target and predictor
 x = data.processed_text.values
 y = data.sentiment.values
+
+#Some commands
+#data.head() to display first 5 lines of each column
+#test2 = [i for i, w in enumerate(data.processed_text) if re.search('(hillari)', w)]
+#test = [w for w in data.processed_text if re.search('(clinton|hillari)', w)]
