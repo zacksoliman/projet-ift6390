7456e0cde770862bce201a4e52de0ff689a46a63
zimmae <maelle.zimmermann@gmail.com>
2016-11-16 13:06:46 -0500

    vectorizing and first bayes classifier

--- Tools/Tweet_analysis.py
+++ Tools/Tweet_analysis.py
@@ -73,13 +73,48 @@ data['processed_text'] = data.text.apply(preprocess)
 data = data.drop_duplicates(subset='processed_text', keep='last')
 
 categories = data.sentiment.unique()
-categories  = categories.tolist()
+categories  = categories.tolist() #neutre, positif, négatif
 
-#set target and predictor
+#set target y and predictor x
 x = data.processed_text.values
-y = data.sentiment.values
+y_labelname = data.sentiment.values
+y_categories = pd.Categorical.from_array(y_labelname)
+y= y_categories.codes #transform sentiment labels into 0,1,2
 
 #Some commands
 #data.head() to display first 5 lines of each column
 #test2 = [i for i, w in enumerate(data.processed_text) if re.search('(hillari)', w)]
 #test = [w for w in data.processed_text if re.search('(clinton|hillari)', w)]
+
+# Déterminer au hasard des indices pour les exemples d'entrainement et de test
+n_train = (data.shape[0]*2) // 3
+inds = [i for i in range(data.shape[0])]
+random.shuffle(inds)
+train_inds = inds[:n_train]
+test_inds = inds[n_train:]
+x_train = x[train_inds]	# garder les bonnes lignes
+x_test = x[test_inds]
+y_train = y[train_inds]
+y_test = y[test_inds]
+
+#bag of words representation: vectorizing
+#1-grams
+count_vect = CountVectorizer() #counts occurences of each word
+X_train_counts = count_vect.fit_transform(x_train)
+tfidf_transformer = TfidfVectorizer() #applies transformation to account for frequency
+X_train_tfidf = tfidf_transformer.fit_transform(x_train)
+#including 2-grams
+bigram_vect = CountVectorizer(ngram_range=(1, 2),token_pattern=r'\b\w+\b', min_df=1)
+X_train_bigramcounts = bigram_vect.fit_transform(x_train)
+
+#First attempt at classification
+clf = MultinomialNB().fit(X_train_counts, y_train)
+clf_bigram = MultinomialNB().fit(X_train_bigramcounts, y_train)
+
+#check with test set
+X_test_counts = count_vect.transform(x_test)
+predicted = clf.predict(X_test_counts)
+np.mean(predicted == y_test)
+X_test_bigramcounts = bigram_vect.transform(x_test)
+predicted2 = clf_bigram.predict(X_test_bigramcounts)
+np.mean(predicted2 == y_test) 
